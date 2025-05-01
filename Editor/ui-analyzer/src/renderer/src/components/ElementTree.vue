f578d14f9118bab372abd6c0a0f3f99b48abb7be
HUHARED <huha.red@gmail.com>
2025-05-01 12:24:10 +0800

    :recycle: <REFACTOR>(UI Analyzer): optimize logic, reset Validate status

--- Editor/ui-analyzer/src/renderer/src/components/ElementTree.vue
+++ Editor/ui-analyzer/src/renderer/src/components/ElementTree.vue
@@ -2,6 +2,7 @@
 <template>
   <v-container class="clean-space flex-column-grow-1 flex-column">
     <v-label class="area-header">Element Tree</v-label>
+
     <v-card v-if="selectorStore.arrEleTree.length === 0" class="ma-1 h-60 pa-3">
       <div>
         Indicate or validate an uia or html element to show its Element Tree.<br />
@@ -30,20 +31,16 @@
             @click="handleNodeClick($event, item.id)"
             >{{ item.title }}</span
           >
+
           <v-tooltip activator="parent" location="bottom">
             <div>
-              {{ JSON.stringify(item.spec, null, 2) }}
-              <br />
-              ------------------------
-              <br />
-              The layer's specification, click to replace the Element Hierarchy.
-              <br />
-              Note:
-              <br />
+              {{ JSON.stringify(item.spec, null, 0) }}<br />
+              ------------------------<br />
+              The layer's specification, click to replace the Element Hierarchy.<br />
+              Note:<br />
               For uia element, you maybe need to add "Index" manually if there are other
               elements with same attributes in its parent element, and add "Depth" manually
-              if you unchecked some layers;
-              <br />
+              if you unchecked some layers;<br />
               For html element, you maybe need to edit or add "childIndex" manually.
             </div>
           </v-tooltip>
@@ -58,16 +55,19 @@ import { VTreeview } from "vuetify/labs/VTreeview";
 
 import { loggerRenderer } from "../ipcOfRenderer";
 import { SelectorNonWindow } from "../../../shared/interface";
-import { useSelectorStore } from "../store";
+import { useSelectorStore, useInformationStore } from "../store";
 
 const selectorStore = useSelectorStore();
+const informationStore = useInformationStore();
 
 function handleNodeClick(_: MouseEvent, id: number): void {
   loggerRenderer.debug("Click Element Tree node " + id);
-  console.log(selectorStore.dictEleTreeSelector[id]);
+  // console.log(selectorStore.dictEleTreeSelector[id]);
 
-  // Reassign to to clean the highlight. Due to the watcher has 300 ms delay, so add delay before add element into arrEleHierarchy again.
   selectorStore.arrEleHierarchy = [];
+  informationStore.$reset();
+
+  /* // Reassign to to clean the highlight. Due to the watcher has 300 ms delay, so add delay before add element into arrEleHierarchy again.
   setTimeout(() => {
     const selectorTemp = selectorStore.dictFromPython["selector"] as SelectorNonWindow;
 
@@ -82,7 +82,21 @@ function handleNodeClick(_: MouseEvent, id: number): void {
     };
 
     selectorStore.updateByDictFromPython();
-  }, 300);
+  }, 300); */
+
+  const selectorTemp = selectorStore.dictFromPython["selector"] as SelectorNonWindow;
+
+  selectorStore.dictFromPython = {
+    selector: {
+      window: selectorStore.dictFromPython["selector"]["window"],
+      category: selectorTemp["category"],
+      specification: selectorStore.dictEleTreeSelector[id],
+    },
+    attributes: {},
+    preview: "",
+  };
+
+  selectorStore.updateByDictFromPython();
 }
 </script>
 
