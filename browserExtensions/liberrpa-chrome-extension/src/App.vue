ad75692eb96d26bf1f81bedd989f887f8a29d20f
HUHARED <huha.red@gmail.com>
2026-04-29 10:58:04 +0800

    :package: <BUILD>(chrome-extension): remove useless vue dependencies

--- browserExtensions/liberrpa-chrome-extension/src/App.vue
+++
@@ -1,6 +0,0 @@
-<!-- FileName: App.vue -->
-<script setup lang="ts"></script>
-
-<template></template>
-
-<style scoped></style>
--- browserExtensions/liberrpa-chrome-extension/vite.config.ts
+++ browserExtensions/liberrpa-chrome-extension/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   build: {
     rollupOptions: {
       input: {
@@ -12,7 +10,7 @@ export default defineConfig({
       },
       output: {
         dir: "dist",
-        format: "esm", 
+        format: "esm",
         entryFileNames: "[name].js",
         chunkFileNames: "[name].js",
         assetFileNames: "[name].[ext]",
