7783624c83e6dab74af6e5a747e409f68818af1e
HUHARED <huha.red@gmail.com>
2026-07-08 09:49:50 +0800

    :bug: <FIX>(Python): improve type check

--- condaLibrary/liberrpa/Common/_WebSocket.py
+++ condaLibrary/liberrpa/Common/_WebSocket.py
@@ -48,7 +48,7 @@ def _normalize_timeout(timeout: object | None) -> int:
     if timeout is None:
         return _INT_TIMEOUT_DEFAULT
 
-    if not isinstance(timeout, int):
+    if not isinstance(timeout, int) or isinstance(timeout, bool):
         raise ValueError("The argument 'timeout' must be an integer.")
 
     if timeout < _CommonValue.INT_TIMEOUT_MIN:
