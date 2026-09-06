73fe7a2e83678fd37e4b87956b185bf33d88f00d
HUHARED <huha.red@gmail.com>
2026-09-06 15:11:39 +0800

    :recycle: <REFACTOR>(executor): safely replace Executor config placeholders

--- electronApplications/executor/src/main/Config/basicConfig.ts
+++ electronApplications/executor/src/main/Config/basicConfig.ts
@@ -9,23 +9,35 @@ import { ensureNonEmptyString, ensureRecord } from "../Common/validation";
 import { strLiberRPAEnvPath } from "./environment";
 import { parseJsonc } from "./jsonc";
 
+function escapeJsonStringContent(strValue: string): string {
+  return JSON.stringify(strValue).slice(1, -1);
+}
+
+function replaceBasicConfigKeywords(
+  strContent: string,
+  strToolName: "BuiltInTools" | "Executor",
+): string {
+  const dictReplaceKeyword: Record<string, string> = {
+    "${LiberRPA}": escapeJsonStringContent(strLiberRPAEnvPath),
+    "${UserName}": escapeJsonStringContent(os.userInfo().username),
+    "${HostName}": escapeJsonStringContent(os.hostname()),
+    "${ToolName}": escapeJsonStringContent(strToolName),
+  };
+
+  let strResult = strContent;
+  for (const [strKeyword, strReplacement] of Object.entries(dictReplaceKeyword)) {
+    strResult = strResult.replaceAll(strKeyword, () => strReplacement);
+  }
+
+  return strResult;
+}
+
 function getBasicConfigOutputLogPath(strToolName: "BuiltInTools" | "Executor"): string {
   const strSettingPath = path.join(strLiberRPAEnvPath, "configFiles/basic.jsonc");
 
   try {
-    let strContent = fs.readFileSync(strSettingPath, { encoding: "utf-8" });
-
-    const dictReplaceKeyword: Record<string, string> = {
-      "${LiberRPA}": strLiberRPAEnvPath.replace(/\\/g, "\\\\"),
-      "${UserName}": os.userInfo().username,
-      "${HostName}": os.hostname(),
-      "${ToolName}": strToolName,
-    };
-
-    for (const [strKeyword, strReplacement] of Object.entries(dictReplaceKeyword)) {
-      const strSafeKeyword = strKeyword.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
-      strContent = strContent.replace(new RegExp(strSafeKeyword, "g"), strReplacement);
-    }
+    const strRawContent = fs.readFileSync(strSettingPath, { encoding: "utf-8" });
+    const strContent = replaceBasicConfigKeywords(strRawContent, strToolName);
 
     const dictConfig = ensureRecord(parseJsonc(strContent, "basic.jsonc"), "Basic config");
     return ensureNonEmptyString(dictConfig.outputLogPath, "Basic config.outputLogPath");
